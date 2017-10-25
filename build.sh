c083f11e3c0191e426f4b472df4fe4d253ceb308
Vultariev <Vultariev@gmail.com>
2017-10-24 23:02:50 -0400

    Improved game framework

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ## Usage: build.sh [OUTPUTFILENAME]
-gcc `sdl2-config --libs --cflags` $1.c -o $1
+gcc `sdl2-config --libs --cflags` -lm $1.c -o $1
--- main.c
+++ main.c
@@ -2,6 +2,66 @@
 #include <stdio.h>
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
+#define PI 3.14159265358979
+struct walls {
+  //Values represent which wall the ball colided with last
+  bool top;
+  bool bottom;
+  bool left;
+  bool right;
+} walls;
+
+struct vec2 {
+  double x;
+  double y;
+};
+
+typedef struct vec2 vec2;
+
+vec2 GetWindowSize(SDL_Window* window)
+{
+  vec2 out;
+  int x,y;
+  SDL_GetWindowSize(window,&x,&y);
+  out.x = x;
+  out.y = y;
+  return out;
+}
+
+void resetWalls(bool *wall)
+{
+  walls.top = false;
+  walls.bottom = false;
+  walls.left = false;
+  walls.right = false;
+  *wall = true;
+}
+
+vec2 ballvel;
+
+double todeg(double rad)
+{
+  return rad/PI*180;
+}
+double torad(double deg)
+{
+  return 180*PI*deg;
+}
+
+void setBallSpeed(double speed, double direction)
+{
+  ballvel.x = sin(torad(direction))*speed;
+  ballvel.y = cos(torad(direction))*speed;
+}
+
+/*double getBallDirection(void)
+{
+  return atan(ballvel.x/ballvel.y);
+}
+double getBallSpeed(void)
+{
+  return ballvel.x/sin(getBallDirection());
+}*/
 
 int main(void)
 {
@@ -9,7 +69,7 @@ int main(void)
 
   SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window* win1 = SDL_CreateWindow("Test",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,400,0);
+  SDL_Window* win1 = SDL_CreateWindow("Test",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,400,SDL_WINDOW_RESIZABLE);
 
   SDL_Renderer* ren = SDL_CreateRenderer(win1,-1,SDL_RENDERER_ACCELERATED);
 
@@ -21,8 +81,11 @@ int main(void)
 
   SDL_Rect balldata;
 
-  balldata.w = 20;
-  balldata.h = 20;
+  double ballspd = 2;
+  double ballang = 45;
+
+  balldata.w = 10;
+  balldata.h = 10;
 
   balldata.x = 0;
   balldata.y = 0;
@@ -30,7 +93,8 @@ int main(void)
 
   SDL_Event event;
 
-  bool ballright = true;
+  ballvel.x = 1;
+  ballvel.y = -1;
 
   while(running)
   {
@@ -47,22 +111,30 @@ int main(void)
         running = false;
       }
     }
-    if(ballright)
+    balldata.x += ballvel.x;
+    balldata.y += ballvel.y;
+    if(balldata.x >= GetWindowSize(win1).x - balldata.w)
     {
-      balldata.x += 1;
-    }
-    else
-    {
-      balldata.x -= 1;
-    }
-    if(balldata.x >=640 - balldata.w)
-    {
-      ballright = false;
+      ballvel.x = -abs(ballvel.x);
+      resetWalls(&walls.right);
     }
     if(balldata.x <= 0)
     {
-      ballright = true;
+      ballvel.x = abs(ballvel.x);
+      resetWalls(&walls.left);
     }
+    if(balldata.y >= GetWindowSize(win1).y - balldata.h)
+    {
+      ballvel.y = -abs(ballvel.y);
+      resetWalls(&walls.top);
+    }
+    if(balldata.y <= 0)
+    {
+      ballvel.y = abs(ballvel.y);
+      resetWalls(&walls.bottom);
+    }
+    setBallSpeed(ballspd,ballang);
+    printf("B: s: %f  d: %f\r",getBallSpeed(),todeg(getBallDirection()));
     SDL_Delay(3);
   }
   end:
